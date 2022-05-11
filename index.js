906946648677a85e0dbe38ac7d3073172bf890c0
Vladlen <ilusenkovladlen@gmail.com>
2022-05-11 14:58:40 +0300

    Added express

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('./src/app');
+
+// TODO: Later to .env
+const APP_PORT = 3000;
+
+app.listen(APP_PORT, () => {
+    console.log(`Now serving your express app at http://localhost:${APP_PORT}`); // eslint-disable-line
+});
