c977a8c6b661bf4f9cc8e73a87fa50926b648313
Vladlen <ilusenkovladlen@gmail.com>
2022-05-11 15:12:12 +0300

    Changed hooks for husky

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({
-        message: 'Hello world!'
-    });
+  res.send({
+    message: 'Hello world!',
+  });
 });
 
 module.exports = app;
