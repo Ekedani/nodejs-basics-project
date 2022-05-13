da97df6006c241268553e5d010314a1c85e24efe
Andrii <ramp8a799@gmail.com>
2022-05-13 17:21:49 +0300

    errors fixed

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const bookRoutes = require('/routes/Book.Route');
-const userRoutes = require('/routes/User.Route');
-const randomBook = require('/routes/RandomBook.Route')
+const bookRoutes = require('../routes/Book.Route');
+const userRoutes = require('../routes/User.Route');
+const randomBook = require('../routes/RandomBook.Route');
 
 const app = express();
 
 app.use('/books', bookRoutes);
-app.use('/random', randomBook)
+app.use('/random', randomBook);
 app.use('/users', userRoutes);
-
 
 app.get('/', (req, res) => {
   res.send({
