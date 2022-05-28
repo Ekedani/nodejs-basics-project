12d8bb430c8814358e3c48939ac27bd68bfde3fc
Vladlen <ilusenkovladlen@gmail.com>
2022-05-28 09:57:18 +0300

    Added error handler

--- src/app.js
+++ src/app.js
@@ -26,4 +26,15 @@ app.use((req, res, next) => {
   next(createError(404, 'Not found'));
 });
 
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message
+    }
+  });
+});
+
 module.exports = app;
