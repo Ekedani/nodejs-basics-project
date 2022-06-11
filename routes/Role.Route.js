8fe90cd0a01a2faf14c48ff18819d639a365849d
Andrii <ramp8a799@gmail.com>
2022-06-11 12:27:20 +0300

    added admin check on Role router

--- routes/Role.Route.js
+++ routes/Role.Route.js
@@ -2,9 +2,11 @@ const express = require('express');
 const RoleController = require('../controllers/Role.Controller');
 const authMiddleware = require('../middlewares/auth');
 
+const isAdmin = require('../middlewares/isAdmin');
+
 const router = express.Router();
 router.use(authMiddleware);
 // TODO: Add admin middleware
-router.get('/', RoleController.getAllRoles);
+router.get('/', isAdmin, RoleController.getAllRoles);
 
 module.exports = router;
